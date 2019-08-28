5fc05d3f0ce1f8fb2d1a88ca697a5d0dd7c7e9d0
Perry Yuan <pyuan@redhat.com>
2019-08-28 04:30:55 -0400

    bitree create

---
+++ BiTree/main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct  BiNode {
+
+    int data;
+    struct BiNode *Left;
+    struct BiNode *Right;
+}BiNode_t;
+
+struct BiNode * newNode(int data){
+
+    struct BiNode* node = (struct BiNode *)malloc(sizeof(struct BiNode));
+    node->data=data;
+    node->Left=NULL;
+    node->Right=NULL;
+    return node;
+
+}
+
+
+
+
+
+int main() {
+
+    int val;
+    printf("Input val:\n");
+    scanf("%d",&val);
+    struct BiNode * root_node=newNode(val);
+    printf("Root Node Data = %d",root_node->data);
+    root_node->Left=newNode(3);
+    root_node->Right=newNode(4);
+    printf("left data:%d",root_node->Left->data);
+    printf("right data:%d",root_node->Right->data);
+
+
+    exit(0);
+}
